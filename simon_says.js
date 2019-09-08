bd77416e58e48a0145dc689554a3fee5ab7d7b33
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-09-08 19:11:01 -0400

    Created final exercise

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,34 @@
-module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+const echo = value => {
+  return value;
+};
+
+const shout = value => {
+  return value.toUpperCase();
+};
+
+const repeat = (value, numOfRepeat) => {
+  let result = "";
+  for (let i = 0; i < numOfRepeat; i++) {
+    result += value + " ";
+  }
+
+  return result.trim();
+};
+
+const startOfWord = (value, numberToReturn) => {
+  if (value.length >= numberToReturn) {
+    return value.substring(0, numberToReturn);
+  }
+};
+
+const firstWord = value => {
+  return value.split(" ")[0];
+};
+
+module.exports = {
+  echo,
+  shout,
+  repeat,
+  startOfWord,
+  firstWord
+};
